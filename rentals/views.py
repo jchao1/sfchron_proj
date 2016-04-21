5d8699ca77f0e6f7c5ded6869a7f22c95d5a36d4
jonathan chao <jlchao23@gmail.com>
2016-04-21 17:01:27 -0500

    added json for listings data modified url structure temporarily, shouldn't affect html development

--- rentals/views.py
+++ rentals/views.py
@@ -1,11 +1,21 @@
+import json
+import os
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.conf import settings
+
+listings = None
+with open(os.path.join(settings.BASE_DIR, 'listings.json')) as f:
+        listings = json.load(f)
+        
+print listings
+
+
 def index(request):
-    #return HttpResponse("Ayyyyy")
 	return render(request, 'rentals/templates/rentals/index.html', {})
 
 
-def listings(request):
-    #return HttpResponse("Ayyyyy")
-	return render(request, 'rentals/templates/rentals/listings.html', {})
+def listings_view(request):
+	return render(request, 'rentals/templates/rentals/listings.html', {'listings': listings })
