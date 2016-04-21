aeb46ccba1c122547f621fd18efc918d7fa2f4f9
jonathan chao <jlchao23@gmail.com>
2016-04-21 17:02:55 -0500

    removed print statement

--- rentals/views.py
+++ rentals/views.py
@@ -9,9 +9,6 @@ from django.conf import settings
 listings = None
 with open(os.path.join(settings.BASE_DIR, 'listings.json')) as f:
         listings = json.load(f)
-        
-print listings
-
 
 def index(request):
 	return render(request, 'rentals/templates/rentals/index.html', {})
